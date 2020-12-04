6421a423325c216d6de10d98ec967b623309135b
Jasmine Carter <jasmine.carter87@gmail.com>
2020-12-04 00:10:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
   return driver;
 }
 
-function deleteFromDriverByKey(driver, key) {
+function destructivelyDeleteFromDriverByKey(driver, key) {
   delete driver.key
 
 }
