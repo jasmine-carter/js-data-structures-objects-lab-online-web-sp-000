bcaa1f8859d0401e4037f71a1fd9b1522e9ef23c
Jasmine Carter <jasmine.carter87@gmail.com>
2020-12-04 00:15:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let driver = {}
 function updateDriverWithKeyAndValue(driver, key, value) {
 //should return a new driver with udpated value for key
   const newDriver = {...driver};
-  newDriver[key] = value ;
+  let newDriver[key] = value ;
 
   return newDriver;
 }
@@ -15,7 +15,7 @@ function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
 }
 
 function deleteFromDriverByKey(driver, key) {
-  newDriver = {...driver};
+  let newDriver = {...driver};
   delete newDriver[key];
   return newDriver;
 }
